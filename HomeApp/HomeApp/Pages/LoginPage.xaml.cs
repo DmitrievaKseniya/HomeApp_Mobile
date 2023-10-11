18e626e40d96e385e2b5e5ff12a9ef5bb1cd105c
DmitrievaKseniya <dkn1933@mail.ru>
2023-10-11 23:08:48 +1000

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        // Константа для текста кнопки
+        public const string BUTTON_TEXT = "Войти";
+        // Переменная счетчика
+        public static int loginCouner = 0;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            // Изменяем внешний вид кнопки для Windows-версии
+            if (Device.RuntimePlatform == Device.UWP)
+                loginButton.CornerRadius = 0;
+        }
+
+        /// <summary>
+        /// По клику обрабатываем счётчик и выводим разные сообщения
+        /// </summary>
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if (loginCouner == 0)
+            {
+                // Если первая попытка - просто меняем сообщения
+                loginButton.Text = $"Выполняется вход..";
+            }
+            else if (loginCouner > 5) // Слишком много попыток - показываем ошибку
+            {
+                // Деактивируем кнопку
+                loginButton.IsEnabled = false;
+                // Получаем последний дочерний элемент, используя свойство Children, затем выполняем распаковку
+                var infoMessage = (Label)stackLayout.Children.Last();
+                // Показываем текстовое сообщение об ошибке
+                infoMessage.Text = "Слишком много попыток! Попробуйте позже.";
+                infoMessage.TextColor = Color.FromRgb(255, 0, 0);
+            }
+            else
+            {
+                // Изменяем текст кнопки и показываем количество попыток входа
+                loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCouner}";
+            }
+
+            // Увеличиваем счетчик
+            loginCouner += 1;
+        }
+    }
+}
